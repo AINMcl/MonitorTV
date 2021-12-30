4741da7a14366bb58a14883984bb3272eb053ca7
Andrés Navarro M <Navarro.Andre@Live.cl>
2021-12-30 14:48:15 -0300

    Home

--- Monitores/Contadores/js/countdownAnoNuevo.js
+++ Monitores/Contadores/js/countdownAnoNuevo.js
@@ -1,8 +1,8 @@
 simplyCountdown('#cuenta', {
-	year: 2022, // required
-	month: 1, // required
-	day: 1, // required
-	hours: 00, // Default is 0 [0-23] integer
+	year: 2021, // required
+	month: 12, // required
+	day: 31, // required
+	hours: 21, // Default is 0 [0-23] integer
 	minutes: 00, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
--- Monitores/Contadores/js/countdownPinera.js
+++ Monitores/Contadores/js/countdownPinera.js
@@ -1,8 +1,8 @@
 simplyCountdown('#cuenta', {
 	year: 2022, // required
 	month: 3, // required
-	day: 11, // required
-	hours: 00, // Default is 0 [0-23] integer
+	day: 10, // required
+	hours: 21, // Default is 0 [0-23] integer
 	minutes: 00, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
