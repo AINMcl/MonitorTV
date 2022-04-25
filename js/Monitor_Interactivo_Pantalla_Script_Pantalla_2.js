64bed6af7ed2ab2f5872649cc0459daa5903010e
Andrés Navarro M <Navarro.Andre@Live.cl>
2022-04-25 14:31:33 -0400

    Monitor, 3, 4, 5, 6

---
+++ js/Monitor_Interactivo_Pantalla_Script_Pantalla_2.js
@@ -0,0 +1,39 @@
+const seccionCanales = document.getElementById("channel-list");
+const btns = seccionCanales.getElementsByClassName("channel");
+
+const nombreBarra = document.createElement('div');
+
+
+const ancla = document.createElement('A');
+ancla.id = 'links';
+ancla.title = 'TITULO DE SEÑAL';
+
+
+for (const canal of LosCanales) {
+    const BtnCanal = document.createElement('BUTTON');
+
+    BtnCanal.classList.add('channel');
+    BtnCanal.innerHTML = canal.nombre;
+
+    BtnCanal.onclick = () => { // https://stackoverflow.com/questions/3316207/add-onclick-event-to-newly-added-element-in-javascript
+        player2.setAttribute(
+            'src',
+            canal.url
+        );
+
+        document.getElementById('video-container2').appendChild(nombreBarra);
+        ancla.innerText = canal.nombre;
+    };
+    seccionCanales.appendChild(BtnCanal)
+}
+
+// https://www.w3schools.com/howto/howto_js_active_element.asp
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        let current = document.getElementsByClassName("SeñalSeleccionada");
+        if (current.length > 0) {
+            current[0].className = current[0].className.replace(" SeñalSeleccionada", "");
+        }
+        this.className += " SeñalSeleccionada";
+    });
+}
