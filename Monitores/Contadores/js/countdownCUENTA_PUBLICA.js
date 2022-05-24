578e968dea8ef6734c00d3349df3f8c8ad070f5d
Andrés Navarro M <Navarro.Andre@Live.cl>
2022-05-23 22:12:41 -0400

    Home

--- Monitores/Contadores/js/countdownCUENTA_PUBLICA.js
+++ Monitores/Contadores/js/countdownCUENTA_PUBLICA.js
@@ -2,7 +2,7 @@ simplyCountdown('#cuenta', {
 	year: 2022, // required
 	month: 6, // required
 	day: 1, // required
-	hours: 15, // Default is 0 [0-23] integer
+	hours: 7, // Default is 0 [0-23] integer
 	minutes: 00, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
