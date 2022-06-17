043f496a09a60c7a152c2d0a70df64b813819cb7
Andrés Navarro M <Navarro.Andre@Live.cl>
2022-06-17 14:22:45 -0400

    Home

---
+++ js/Monitor_Interactivo_Pantalla_Señales_Twitch.js
@@ -0,0 +1,161 @@
+let LosCanales = [
+    //TWITCH VIDEO
+    {
+        'titulo': 'IBAI',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">tv</span>IBAI',
+        'url': 'https://player.twitch.tv/?channel=ibai&parent=ainmcl.github.io'
+    }, {
+        'titulo': 'AUROPLAY',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">tv</span>AURONPLAY',
+        'url': 'https://player.twitch.tv/?channel=auronplay&parent=ainmcl.github.io'
+    },{
+        'titulo': 'RUBIUS',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">tv</span>RUBIUS',
+        'url': 'https://player.twitch.tv/?channel=rubius&parent=ainmcl.github.io'
+    },{
+        'titulo': 'THE FOCUS',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">tv</span>THE FOCUS',
+        'url': 'https://player.twitch.tv/?channel=xxxthefocusxxx&parent=ainmcl.github.io'
+    },{
+        'titulo': 'AXOZER',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">tv</span>AXOZER',
+        'url': 'https://player.twitch.tv/?channel=axozer&parent=ainmcl.github.io'
+    },{
+        'titulo': 'THE GREFG',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">tv</span>THE GREFG',
+        'url': 'https://player.twitch.tv/?channel=thegrefg&parent=ainmcl.github.io'
+    },{
+        'titulo': 'JUAN GUARNIZO',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">tv</span>JUAN GUARNIZO',
+        'url': 'https://player.twitch.tv/?channel=juansguarnizo&parent=ainmcl.github.io'
+    },{
+        'titulo': 'DEQIUV',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">tv</span>DEQIUV',
+        'url': 'https://player.twitch.tv/?channel=deqiuv&parent=ainmcl.github.io'
+    },{
+        'titulo': 'KARCHEZ',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">tv</span>KARCHEZ',
+        'url': 'https://player.twitch.tv/?channel=karchez&parent=ainmcl.github.io'
+    },{
+        'titulo': 'LUZU',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">tv</span>LUZU',
+        'url': 'https://player.twitch.tv/?channel=luzu&parent=ainmcl.github.io'
+    },{
+        'titulo': 'POLISPOL',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">tv</span>POLISPOL',
+        'url': 'https://player.twitch.tv/?channel=polispol1&parent=ainmcl.github.io'
+    },{
+        'titulo': 'CAROLA',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">tv</span>CAROLA',
+        'url': 'https://player.twitch.tv/?channel=carola&parent=ainmcl.github.io'
+    },{
+        'titulo': 'BIYIN',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">tv</span>BIYIN',
+        'url': 'https://player.twitch.tv/?channel=biyin_&parent=ainmcl.github.io'
+    },{
+        'titulo': 'COPANO',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">tv</span>COPANO',
+        'url': 'https://player.twitch.tv/?channel=copano&parent=ainmcl.github.io'
+    },
+    
+    
+    {
+        'titulo': 'ROBERTTSON',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">tv</span>ROBERTTSON',
+        'url': 'https://player.twitch.tv/?channel=roberttson&parent=ainmcl.github.io'
+    },{
+        'titulo': 'RDRONE UY',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">tv</span>RDRONE UY',
+        'url': 'https://player.twitch.tv/?channel=rdroneuy&parent=ainmcl.github.io'
+    },{
+        'titulo': 'SPACEX STORM',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">tv</span>SPACEX STORM',
+        'url': 'https://player.twitch.tv/?channel=spacexstorm&parent=ainmcl.github.io'
+    }, {
+        'titulo': 'FENIX REVIEW',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">tv</span>FENIX',
+        'url': 'https://player.twitch.tv/?channel=fenixreview&parent=ainmcl.github.io'
+    }, {
+        'titulo': 'JACK WALLACE',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">tv</span>JACK',
+        'url': 'https://player.twitch.tv/?channel=unjackwallace&parent=ainmcl.github.io'
+    },
+    //TWITCH CHAT
+    {
+        'titulo': 'IBAI',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">forum</span>IBAI',
+        'url': 'https://www.twitch.tv/embed/ibai/chat?parent=ainmcl.github.io'
+    }, {
+        'titulo': 'AUROPLAY',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">forum</span>AURONPLAY',
+        'url': 'https://www.twitch.tv/embed/auronplay/chat?parent=ainmcl.github.io'
+    },{
+        'titulo': 'RUBIUS',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">forum</span>RUBIUS',
+        'url': 'https://www.twitch.tv/embed/rubius/chat?parent=ainmcl.github.io'
+    },{
+        'titulo': 'THE FOCUS',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">forum</span>THE FOCUS',
+        'url': 'https://www.twitch.tv/embed/xxxthefocusxxx/chat?parent=ainmcl.github.io'
+    },{
+        'titulo': 'AXOZER',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">forum</span>AXOZER',
+        'url': 'https://www.twitch.tv/embed/axozer/chat?parent=ainmcl.github.io'
+    },{
+        'titulo': 'THE GREFG',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">forum</span>THE GREFG',
+        'url': 'https://www.twitch.tv/embed/thegrefg/chat?parent=ainmcl.github.io'
+    },{
+        'titulo': 'JUAN GUARNIZO',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">forum</span>JUAN GUARNIZO',
+        'url': 'https://www.twitch.tv/embed/juansguarnizo/chat?parent=ainmcl.github.io'
+    },{
+        'titulo': 'DEQIUV',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">forum</span>DEQIUV',
+        'url': 'https://www.twitch.tv/embed/deqiuv/chat?parent=ainmcl.github.io'
+    },{
+        'titulo': 'KARCHEZ',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">forum</span>KARCHEZ',
+        'url': 'https://www.twitch.tv/embed/karchez/chat?parent=ainmcl.github.io'
+    },{
+        'titulo': 'LUZU',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">forum</span>LUZU',
+        'url': 'https://www.twitch.tv/embed/luzu/chat?parent=ainmcl.github.io'
+    },{
+        'titulo': 'POLISPOL',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">forum</span>POLISPOL',
+        'url': 'https://www.twitch.tv/embed/polispol1/chat?parent=ainmcl.github.io'
+    },{
+        'titulo': 'CAROLA',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">forum</span>CAROLA',
+        'url': 'https://www.twitch.tv/embed/carola/chat?parent=ainmcl.github.io'
+    },{
+        'titulo': 'BIYIN',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">forum</span>BIYIN',
+        'url': 'https://www.twitch.tv/embed/biyin_/chat?parent=ainmcl.github.io'
+    },{
+        'titulo': 'COPANO',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">forum</span>COPANO',
+        'url': 'https://www.twitch.tv/embed/copano/chat?parent=ainmcl.github.io'
+    },{
+        'titulo': 'ROBERTTSON',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">forum</span>ROBERTTSON',
+        'url': 'https://www.twitch.tv/embed/roberttson/chat?parent=ainmcl.github.io'
+    },{
+        'titulo': 'RDRONE UY',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">forum</span>RDRONE UY',
+        'url': 'https://www.twitch.tv/embed/rdroneuy/chat?parent=ainmcl.github.io'
+    },{
+        'titulo': 'SPACEX STORM',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">forum</span>SPACEX STORM',
+        'url': 'https://www.twitch.tv/embed/spacexstorm/chat?parent=ainmcl.github.io'
+    }, {
+        'titulo': 'FENIX REVIEW',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">forum</span>FENIX',
+        'url': 'https://www.twitch.tv/embed/fenixreview/chat?parent=ainmcl.github.io'
+    }, {
+        'titulo': 'JACK WALLACE',
+        'nombre': '<img style="height: 20px; width: auto; margin-right: 10px;" src="imagenes/Logo_Twitch.svg"></img><span style="margin-right: 10px;" class="material-icons">forum</span>JACK',
+        'url': 'https://www.twitch.tv/embed/unjackwallace/chat?parent=ainmcl.github.io'
+    }
+]
